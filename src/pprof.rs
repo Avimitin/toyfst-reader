dd3359b1442d70e137d193b628187bd703bb1f25
Avimitin <dev@avimit.in>
2024-01-17 18:59:20 +0800

    Add new StringTable implementation to help allocating string
    Signed-off-by: Avimitin <dev@avimit.in>

--- src/pprof.rs
+++ src/pprof.rs
@@ -1,4 +1,49 @@
-include!(concat!(env!("OUT_DIR"), "/perftools.profiles.rs"));
+use std::collections::HashMap;
+
+// ProtoBuf struct defined in target/<debug/release>/build/<name>/out
+include!(concat!(env!("OUT_DIR"), "/pprof.profiles.rs"));
+
+/// The protobuf definition given by pprof specified that every string usage needs to be store in
+/// an array, and all the string fields should be an index to the field in the String Table.
+///
+/// This struct wraps a HashMap for quick string searching and insertion.
+#[derive(Debug, Default)]
+pub struct StringTable {
+  data: HashMap<String, i64>,
+  next: i64,
+}
+
+impl StringTable {
+  /// Return a default empty StringTable.
+  pub fn new() -> Self {
+    // According to pprof spec, the first item of the string table should left blank.
+    Self {
+      data: HashMap::from([(String::new(), 0)]),
+      next: 1,
+    }
+  }
+
+  /// Return the id of the given string. If the string doesn't exists in StringTable, then it will
+  /// be allocated and assigned a new ID.
+  pub fn id(&mut self, q: &str) -> i64 {
+    self
+      .data
+      .entry(q.to_string())
+      .or_insert_with(|| {
+        let n = self.next;
+        self.next += 1;
+        n
+      })
+      .to_owned()
+  }
+
+  /// Convert the StringTable struct to a list of strings.
+  pub fn to_string_table(&self) -> Vec<String> {
+    let mut cache = self.data.iter().collect::<Vec<_>>();
+    cache.sort_by(|(_, prev), (_, next)| prev.cmp(next));
+    cache.into_iter().map(|(str, _)| str.to_owned()).collect()
+  }
+}
 
 #[test]
 fn sample_profile() {
@@ -8,13 +53,18 @@ fn sample_profile() {
 
   use prost::Message;
 
+  let mut str_tbl = StringTable::new();
+
   let mut p = Profile::default();
   p.time_nanos = 10000;
-  p.string_table = ["", "cpu", "cycle", "property", "property A"]
-    .map(|s| s.to_string())
-    .to_vec();
-  p.period_type = Some(ValueType { r#type: 1, unit: 2 });
-  p.sample_type = vec![ValueType { r#type: 1, unit: 2 }];
+  p.period_type = Some(ValueType {
+    r#type: str_tbl.id("cycle"),
+    unit: str_tbl.id("number"),
+  });
+  p.sample_type = vec![ValueType {
+    r#type: str_tbl.id("signal"),
+    unit: str_tbl.id("value"),
+  }];
   p.period = 1;
   p.duration_nanos = 1000_000_000;
 
@@ -24,6 +74,9 @@ fn sample_profile() {
   let mut label = Label::default();
   label.key = 3;
   label.str = 4;
+
+  p.string_table = str_tbl.to_string_table();
+  dbg!(&p);
 
   let mut buf = Vec::new();
   buf.reserve(p.encoded_len());
